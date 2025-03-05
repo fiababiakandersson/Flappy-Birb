a02338007b9dbe2dfb7625a1ffc1c50d14c6dff6
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2025-03-05 17:34:22 +0100

    testing

--- core/src/se/yrgo/game/AlienGame.java
+++ core/src/se/yrgo/game/AlienGame.java
@@ -1,12 +1,12 @@
 package se.yrgo.game;
 
-import com.badlogic.gdx.Game;
+import com.badlogic.gdx.*;
 
 /**
  * This is the class for the main game. It controlls the different
  * screens that exist. Screens lets us divide our program into
  * separate entities that need different handling such as
- * menus, different game modes, end screens etc.
+ * menus, different game modes, end screens etc
  */
 public class AlienGame extends Game {
 	private GameScreen gameScreen;
@@ -15,16 +15,16 @@ public class AlienGame extends Game {
 	private int points;
 
 	@Override
-    public void create () {
+	public void create() {
 		gameScreen = new GameScreen(this);
 		gameOverScreen = new GameOverScreen(this);
-        newGame();
-    }
+		newGame();
+	}
 
-    @Override
-    public void dispose () {
-        gameScreen.dispose();
-    }
+	@Override
+	public void dispose() {
+		gameScreen.dispose();
+	}
 
 	public void addPoints(int points) {
 		this.points += points;
