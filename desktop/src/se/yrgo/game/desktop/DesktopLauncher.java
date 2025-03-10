667080d22b7a45da18200f760c043a8fba3ae581
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2025-03-10 09:19:50 +0100

    change game screen dimensions

--- desktop/src/se/yrgo/game/desktop/DesktopLauncher.java
+++ desktop/src/se/yrgo/game/desktop/DesktopLauncher.java
@@ -1,15 +1,14 @@
 package se.yrgo.game.desktop;
 
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.*;
 
 import se.yrgo.game.*;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		// this is where we configure how our program starts on the desktop
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setWindowedMode(800, 400);
+		config.setWindowedMode(390, 844);
 		new Lwjgl3Application(new AlienGame(), config);
 	}
 }
