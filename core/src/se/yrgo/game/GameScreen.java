e156a14f3b81c1b545ea863decff5d6cfef86cf9
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2025-04-02 10:20:54 +0200

    remove glyphLayout

--- core/src/se/yrgo/game/GameScreen.java
+++ core/src/se/yrgo/game/GameScreen.java
@@ -33,7 +33,6 @@ public class GameScreen extends ScreenAdapter implements InputProcessor {
     private List<AnimatedSprite> planets;
     private List<AnimatedSprite> backgroundStars; // For background stars
     private BitmapFont font;
-    private GlyphLayout glyphLayout;
     private String[] planetsArr = { "bloodMoon.png", "earth.png", "jupiter.png", "mars.png", "moon.png", "venus.png" };
 
     private static final float GRAVITY = -2700f;
@@ -56,7 +55,6 @@ public class GameScreen extends ScreenAdapter implements InputProcessor {
         this.planets = new ArrayList<>();
         this.backgroundStars = new ArrayList<>();
         this.font = new BitmapFont();
-        this.glyphLayout = new GlyphLayout();
         font.getData().setScale(2);
         font.setColor(Color.WHITE);
 
